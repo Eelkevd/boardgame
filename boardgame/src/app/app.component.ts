ac35389b32e46c1e87cd16a245e4c5fc874aaac6
Eelke van Dijk <eelkevandijk@eelkes-mbp.groningen.nl.ibm.com>
2025-03-14 15:41:53 +0100

    converted game options to dropdown with slight animations

--- boardgame/src/app/app.component.ts
+++ boardgame/src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { GameOptionsMenuComponent } from './game-options-menu/game-options-menu.component';
 import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GameboardComponent, GameOptionsMenuComponent, FormsModule],
+  imports: [RouterOutlet, GameboardComponent, GameOptionsMenuComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   tileCount: number = 10;
+  showMenu: boolean = false;
 
   updateTileCount(newCount: number) {
     this.tileCount = newCount;
   }
-  title = 'boardgame';
+  // title = 'boardgame';
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
