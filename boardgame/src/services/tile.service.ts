b46e5d2dde061cedebf98eeac5fb9da954ab3e3e
Eelke van Dijk <eelkevandijk@dhcp-9-80-24-129.groningen.nl.ibm.com>
2025-03-25 17:04:04 +0100

    added service to controle tile count

---
+++ boardgame/src/services/tile.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TileService {
+
+  private tileCountTracker = new BehaviorSubject<number>(10);
+  tileCount$ = this.tileCountTracker.asObservable();
+
+  setTileCount(count: number) {
+    this.tileCountTracker.next(count);
+  }
+}
